82e3687645382f909d6049ae1636471bdb04d2ca
Squirrel-314 <schoolarlybay@gmail.com>
2021-01-14 17:46:52 -0500

    Back Button!

--- main.js
+++ main.js
@@ -1,9 +1,18 @@
-// document.getElementById("vegetable-dash").scrollIntoView();
+let backButton = document.getElementById("back");
 
-function page1() {
-   document.location = '#meet';
+if (window.location.href != "https://meetsquirrel.github.io" || "https://meetsquirrel.github.io#meet") {
+   backButton.style.opacity = "1";
 }
 
-function page2() {
-   document.location = '#vegetable-dash';
+function next() {
+   if (window.location.href == "https://meetsquirrel.github.io/index.html#vegetable-dash") {
+      document.location = "#vegetable-dash";
+   }
+}
+
+function last() {
+   if (window.location.href == "https://meetsquirrel.github.io/index.html#vegetable-dash" || "file:///D:/Documents/GitHub/meetsquirrel.github.io/index.html#vegetable-dash") {
+      document.location = "#meet";
+      backButton.style.opacity = "0";
+   }
 }
