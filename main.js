ce956ec9e430e0b256348aa52d84e447e3f70c7e
Squirrel-314 <schoolarlybay@gmail.com>
2021-01-14 18:08:14 -0500

    Actually Fixed Buttons!

--- main.js
+++ main.js
@@ -5,14 +5,21 @@ if (window.location.href != "https://meetsquirrel.github.io" || "https://meetsqu
 }
 
 function next() {
-   if (window.location.href == "https://meetsquirrel.github.io/index.html#vegetable-dash") {
+   if (window.location.href == "https://meetsquirrel.github.io/index.html" || "file:///D:/Documents/GitHub/meetsquirrel.github.io/index.html#meet") {
       document.location = "#vegetable-dash";
+   }
+   if (window.location.href == "https://meetsquirrel.github.io/index.html#vegetable-dash") {
+      document.location = "#coming-soon";
    }
 }
 
 function last() {
-   if (window.location.href == "https://meetsquirrel.github.io/index.html#vegetable-dash" || "file:///D:/Documents/GitHub/meetsquirrel.github.io/index.html#vegetable-dash") {
+   if (window.location.href == "https://meetsquirrel.github.io/index.html#vegetable-dash") {
       document.location = "#meet";
       backButton.style.opacity = "0";
    }
+   if (window.location.href == "https://meetsquirrel.github.io/index.html#coming-soon") {
+      document.location = "#vegetable-dash";
+   }
 }
+coming-soon
