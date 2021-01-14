708089294240ff2c450ec28f860067df5f201cd7
Squirrel-314 <schoolarlybay@gmail.com>
2021-01-14 14:40:08 -0500

    Still Working on Second Page!

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
 // document.getElementById("vegetable-dash").scrollIntoView();
+
+function page1() {
+   document.location = '#meet';
+}
+
+function page2() {
+   document.location = '#vegetable-dash';
+}
