0abd46a7a7968f5916f7d1a0f89363e12853ed2e
Squirrel-314 <schoolarlybay@gmail.com>
2021-01-14 20:11:21 -0500

    Working on it!

--- main.js
+++ main.js
@@ -8,13 +8,13 @@ function next() {
    if (window.location.href == "https://meetsquirrel.github.io/index.html" || "file:///D:/Documents/GitHub/meetsquirrel.github.io/index.html#meet") {
       document.location = "#vegetable-dash";
    }
-   if (window.location.href == "https://meetsquirrel.github.io/index.html#vegetable-dash") {
+   if (window.location.href == "https://meetsquirrel.github.io/#vegetable-dash") {
       document.location = "#coming-soon";
    }
 }
 
 function last() {
-   if (window.location.href == "https://meetsquirrel.github.io/index.html#vegetable-dash") {
+   if (window.location.href == "https://meetsquirrel.github.io/#vegetable-dash") {
       document.location = "#meet";
       backButton.style.opacity = "0";
    }
