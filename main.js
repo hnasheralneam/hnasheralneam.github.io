482dca557c7f3d9ec9d697ccc8e64dc4c5665cc9
Squirrel-314 <schoolarlybay@gmail.com>
2021-01-15 12:55:14 -0500

    Finally got the back and forward buttons to work! (else if)

--- main.js
+++ main.js
@@ -1,24 +1,42 @@
+let pageLocation = "meet";
 let backButton = document.getElementById("back");
-
-if (window.location.href != "https://meetsquirrel.github.io" || "https://meetsquirrel.github.io#meet") {
-   backButton.style.opacity = "1";
-}
+let nextButton = document.getElementById("next");
 
 function next() {
-   if (window.location.href == "https://meetsquirrel.github.io/index.html" || "file:///D:/Documents/GitHub/meetsquirrel.github.io/index.html#meet") {
+   if (pageLocation === "meet") {
       document.location = "#vegetable-dash";
+      pageLocation = "vegetable-dash";
    }
-   if (window.location.href == "https://meetsquirrel.github.io/#vegetable-dash") {
+   else if (pageLocation === "vegetable-dash") {
       document.location = "#coming-soon";
+      pageLocation = "coming-soon";
    }
+   buttonVisiblility();
 }
 
 function last() {
-   if (window.location.href == "https://meetsquirrel.github.io/#vegetable-dash") {
+   if (pageLocation === "vegetable-dash") {
       document.location = "#meet";
+      pageLocation = "meet";
+   }
+   else if (pageLocation === "coming-soon") {
+      document.location = "#vegetable-dash";
+      pageLocation = "vegetable-dash";
+   }
+   buttonVisiblility();
+}
+
+function buttonVisiblility() {
+   if (pageLocation != "meet") {
+      backButton.style.opacity = "1";
+   }
+   else {
       backButton.style.opacity = "0";
    }
-   if (window.location.href == "https://meetsquirrel.github.io/index.html#coming-soon") {
-      document.location = "#vegetable-dash";
+   if (pageLocation != "coming-soon") {
+      nextButton.style.opacity = "1";
+   }
+   else {
+      nextButton.style.opacity = "0";
    }
 }
